851b0541fa55a1db033f6f98e84f3371284597bc
Vahan Ghazaryan <vaghazar@c1r1s1.42yerevan.am>
2022-09-27 11:34:50 +0400

    libft

---
+++ includes/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vaghazar <vaghazar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 16:31:58 by vaghazar          #+#    #+#             */
+/*   Updated: 2022/09/21 16:40:48 by vaghazar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "../libft/libft.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+
+void    dis_prompt();
+
+#endif
+
