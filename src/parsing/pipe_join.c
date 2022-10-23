5d315e762dc24bf6a19afd3be5d652e6394e01d6
Aghasy <aghasghazaryan8888@gmail.com>
2022-10-23 23:52:06 +0400

    V_test

--- src/parsing/pipe_join.c
+++ src/parsing/pipe_join.c
@@ -27,16 +27,18 @@ int	pipe_join(t_parse *parser)
 	parser->join_pipe = malloc(sizeof(char *) * (parser->l_arr + 1));
 	if (parser->join_pipe == NULL && !ft_perror("minishell"))
 		return (0);
-    fill_null((void *)&parser->join_pipe, parser->l_arr + 1);
+	fill_null((void *)&parser->join_pipe, parser->l_arr + 1);
 	while (parser->spl_pipe && parser->spl_pipe[i] != NULL)
 	{
-		while (parser->spl_pipe && parser->spl_pipe[i] && parser->spl_pipe[i][0] != '|')
+		while (parser->spl_pipe && parser->spl_pipe[i]
+			&& parser->spl_pipe[i][0] != '|')
 		{
-			parser->join_pipe[k] = ft_strjoin(parser->join_pipe[k], parser->spl_pipe[i++]);
+			parser->join_pipe[k] = ft_strjoin(parser->join_pipe[k],
+												parser->spl_pipe[i++]);
 		}
 		k++;
 		if (parser->l_arr == k)
-				parser->join_pipe = resize_arr(parser->join_pipe, &parser->l_arr);
+			parser->join_pipe = resize_arr(parser->join_pipe, &parser->l_arr);
 		if (parser->spl_pipe[i])
 			i++;
 	}
